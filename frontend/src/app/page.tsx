3b753c159b794af350c081a03f951912fff9721b
potakaaa <helbirog@gmail.com>
2025-02-15 23:45:50 +0800

    added image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data: session, status } = useSession();
@@ -17,6 +18,13 @@ export default function Home() {
           <p>Signed in</p>
           <p>{session?.user?.name}</p>
           <p>{session?.user?.email}</p>
+          <Image
+            src={session?.user?.image || "/default-image.png"}
+            width={100}
+            height={100}
+            alt={"image"}
+            className="rounded-full shadow-md"
+          />
         </div>
       )}
     </div>
